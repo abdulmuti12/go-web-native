aa9090ed0ea76faaf5c31e5af93f9b6f0c10a6b7
abdulmuti12 <abdulmfmuti@gmail.com>
2024-05-10 20:50:36 +0700

    Add product detail

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"go-web-native/config"
 	"go-web-native/controllers/categorycontroller"
 	"go-web-native/controllers/homecontroller"
+	"go-web-native/controllers/productcontroller"
 	"log"
 	"net/http"
 )
@@ -14,11 +15,16 @@ func main() {
 
 	http.HandleFunc("/", homecontroller.Welcome)
 
-	// 2. Category
 	http.HandleFunc("/categories", categorycontroller.Index)
 	http.HandleFunc("/categories/add", categorycontroller.Add)
 	http.HandleFunc("/categories/edit", categorycontroller.Edit)
 	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
 
 	log.Println("Server running 8081")
 	http.ListenAndServe(":8081", nil)
